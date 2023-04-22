7710cc32ce03c07be86de55728e0e9400f34ebe7
Brian Law <gmbrianlaw@gmail.com>
2023-04-21 22:22:51 -0700

    Transition to tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+    content: ["./index.html"],
+
+    theme: {
+        extend: {
+            colors: {
+                'bg': '#262626',
+                'text': '#d4d4d4'
+            }
+        }
+    }
+
+};
