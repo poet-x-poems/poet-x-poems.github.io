b294ed5685e7c5b4a112699465501755dde6bd83
Brian Law <gmbrianlaw@gmail.com>
2023-04-27 18:56:50 -0700

    Try to troll myself with Javascript

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-setTimeout(
-    () => {
-        const e = document.getElementById("test");
-        e.innerText = "Goodbye!";
-    },
-    2000
-);
+class Poem {
+
+    constructor(name, text) {
+
+        this.name = name;
+        this.text = text;
+
+    }
+
+}
+
+const title = document.getElementById("poem-title");
+const text = document.getElementById("poem-text");
+const poems = [];
+
+poems.push(new Poem("Sample", "Lorem Ipsum..."));
+
+function display(poem) {
+
+    title.innerText = poem.name;
+    text.innerText = poem.text;
+
+}
+
+display(poems[0]);
